8e557f2b06f8c3dd461f55bf7aefd2b1d5d3da66
luca.salzone <luca.salzone@gmail.com>
2017-03-25 01:07:00 +0100

    Crude with Resource

--- src/main/webapp/js/app/app.js
+++ src/main/webapp/js/app/app.js
@@ -1,19 +1,52 @@
 // Define application
-var myapp = angular.module('app', []);
+var myapp = angular.module('app', ['ngResource']);
 
-// Define the `ctrlApp` controller on the `phonecatApp` module
-myapp.controller('ctrl', function ctrlApp($scope, $http) {
-	$scope.name="luca";
-	$scope.utente;
-	$scope.searchUser=function(){
-		$http({
-			method : 'GET',
-			url : 'http://127.0.0.1:8080/test/api/hello/utenti/'+$scope.name
-		}).then(function successCallback(response) {
-			$scope.utente = response.data;
-		}, function errorCallback(response) {
-			alert("Errore!" + response);
-		});
+myapp.factory('UserFactory', function($resource){
+	return $resource('http://127.0.0.1:8080/test/api/hello/utenti/:name');
+});
+
+myapp.controller('ctrl', function ctrlApp($scope, UserFactory){ //  $http,$resource,restFactory) {
+	
+	$scope.userList=function(){
+		$scope.users = UserFactory.query();
 	}
-	$scope.searchUser();
+	$scope.userList();
+	
+	$scope.searchUser=function(){
+		$scope.user = UserFactory.get({'name':$scope.selectName});
+	}
+	$scope.addUser=function(){
+		UserFactory.save(
+			{'name' :$scope.newUser.name,'user':$scope.newUser},
+			function(result){
+				if(result.$resolved){
+					alert("success");
+					$scope.userList();
+				}
+			}
+		);
+		
+	}
+	$scope.updateUser=function(userValue){
+		UserFactory.save(
+			{'name' :userValue.name,'user':userValue},
+			function(result){
+				if(result.$resolved){
+					alert("success");
+				}
+			}
+		);
+	}
+	$scope.deleteUser=function(user){
+		UserFactory.delete(
+			{'user' :user},
+			function(result){
+				if(result.$resolved){
+					$scope.user=undefined;
+					alert("success");
+					$scope.userList();
+				}
+			}
+		);
+	}
 });
